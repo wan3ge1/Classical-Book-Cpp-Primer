455204e97f27f779254ba24425ea2edbc64c0370
wan3ge1 <182803721@qq.com>
2021-05-14 16:44:34 +0800

    reform directories

--- lists/lists/hexoct.cpp
+++ lists/lists/hexoct.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 
-int main()
-{
+int main() {
+
 	using namespace std;
 
-	int var_dec(42);
-	int var_hex(0x42);
-	int var_oct(042);
+	int var_dec = 42;
+	int var_hex = 42;
+	int var_oct = 42;
 
 	cout << "var_dec:" << var_dec << "\n";
+	cout << hex;
 	cout << "var_hex:" << var_hex << "\n";
+	cout << oct;
 	cout << "var_oct:" << var_oct << "\n";
 
 	return 0;
-
 }
